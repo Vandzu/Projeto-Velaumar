d6da9eaadda4b48d769680ddaa9b072a7942ae92
Vandzu <65266125+Vandzu@users.noreply.github.com>
2022-01-12 17:18:14 -0300

    Commit Checkpoint 3 Projeto Integrado

--- public/src/controller/DocsUserController.js
+++ public/src/controller/DocsUserController.js
@@ -11,13 +11,6 @@ this.filters();
 
 connectFirebase() {
 const firebaseConfig = {
-    apiKey: "AIzaSyBtJ3R8DWl26qtCXDQvCO0TU3dC5FRjj8s",
-    authDomain: "velaumar-c0b81.firebaseapp.com",
-    databaseURL: "https://velaumar-c0b81-default-rtdb.firebaseio.com",
-    projectId: "velaumar-c0b81",
-    storageBucket: "velaumar-c0b81.appspot.com",
-    messagingSenderId: "12998876707",
-    appId: "1:12998876707:web:c1de580b4006454ecc5b96"
 };
 
 // Initialize Firebase
