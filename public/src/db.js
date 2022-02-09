373a68a1051f00042e59beddf31eb50c6ea489d9
Vandzu <65266125+Vandzu@users.noreply.github.com>
2022-02-09 19:01:29 -0300

    ajustes

--- public/src/db.js
+++ public/src/db.js
@@ -5,13 +5,7 @@ export default class Firebase {
 
     connectFirebase() {
         const firebaseConfig = {
-            apiKey: "AIzaSyBtJ3R8DWl26qtCXDQvCO0TU3dC5FRjj8s",
-            authDomain: "velaumar-c0b81.firebaseapp.com",
-            databaseURL: "https://velaumar-c0b81-default-rtdb.firebaseio.com",
-            projectId: "velaumar-c0b81",
-            storageBucket: "velaumar-c0b81.appspot.com",
-            messagingSenderId: "12998876707",
-            appId: "1:12998876707:web:c1de580b4006454ecc5b96"
+
         };
         
         // Initialize Firebase
